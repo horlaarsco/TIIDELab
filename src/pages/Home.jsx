9f5b11ac9b2111ca8006c8c23866452711b6836d
Darey <horla@Dareys-MacBook-Pro.local>
2020-06-29 20:05:46 +0100

    controls

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -203,10 +203,9 @@ export const Home = () => {
         </div>
         <Testimonial>
           <div
-            id='carouselExampleSlidesOnly'
+            id='carouselExampleControls'
             className='carousel slide'
             data-ride='carousel'
-            data-interval='5000'
           >
             <div className='carousel-inner'>
               <div className='carousel-item active'>
@@ -230,62 +229,89 @@ export const Home = () => {
               </div>
               <div className='carousel-item '>
                 <p>
-                  Tiidelab has helped me gain more focus and shed more light on
-                  what I want to do and how to go about it. Within the space of
-                  five months, I've been able to learn how to look for growing
-                  problems in the society, how to come up with a solution and
-                  how to create the solution using tech. I've improved not only
-                  as a developer and a business personnel but also as a person.
+                  At TIIDELab, I had an extensive learning experience about the
+                  requisite tech and non-tech skills for survival in the tech
+                  industry. I had the opportunity to meet wonderful people,
+                  learned a new Javascript Framework and conquered my fears
+                  about CLI Environment. With TIIDELab experience, my career in
+                  the software space has been given a leap to everlasting
+                  growth.
                 </p>
-                <h2>Sanusi Micheal HAYATU</h2>
+                <h2>Christainah Oluwakemi OWOYELE</h2>
                 <p>
                   <i>-Former student/Lead Engineer at Apple</i>
                 </p>
                 <img
                   width='100px'
-                  src='https://res.cloudinary.com/ddxgfwoao/image/upload/v1593454384/sanusi_Pasport_strd9b.png'
+                  src='https://res.cloudinary.com/ddxgfwoao/image/upload/v1593457186/girl_head_ylqddg.png'
                   alt=''
                 />
               </div>
               <div className='carousel-item '>
                 <p>
-                  Tiidelab has helped me gain more focus and shed more light on
-                  what I want to do and how to go about it. Within the space of
-                  five months, I've been able to learn how to look for growing
-                  problems in the society, how to come up with a solution and
-                  how to create the solution using tech. I've improved not only
-                  as a developer and a business personnel but also as a person.
+                  Joining TIIDELab Fellowship was a dream come true in my tech
+                  journey, it gave me the opportunity to view/understand the
+                  Tech world better, now I can confidently call myself a junior
+                  developer because I have gotten foundation and a path to
+                  follow in growing into a top senior developer in the nearest
+                  future.
                 </p>
-                <h2>Sanusi Micheal HAYATU</h2>
+                <h2>Godwin JACOB</h2>
                 <p>
                   <i>-Former student/Lead Engineer at Apple</i>
                 </p>
                 <img
                   width='100px'
-                  src='https://res.cloudinary.com/ddxgfwoao/image/upload/v1593454384/sanusi_Pasport_strd9b.png'
+                  src='https://res.cloudinary.com/ddxgfwoao/image/upload/v1593457179/godwin_head_cie1kt.png'
                   alt=''
                 />
               </div>
               <div className='carousel-item '>
                 <p>
-                  Tiidelab has helped me gain more focus and shed more light on
-                  what I want to do and how to go about it. Within the space of
-                  five months, I've been able to learn how to look for growing
-                  problems in the society, how to come up with a solution and
-                  how to create the solution using tech. I've improved not only
-                  as a developer and a business personnel but also as a person.
+                  I had basic knowledge of HTML and CSS before TIIDELab and
+                  looking back, I really did not know how vast software
+                  development was. At the fellowship, we were taught front end
+                  web development technologies such as HTML, CSS and JavaScript
+                  and VUE JS as a JavaScript framework. We also learnt back end
+                  web development technologies such as Node JS using Express JS
+                  as its framework and also a database management system, which
+                  is MySQL.
                 </p>
-                <h2>Sanusi Micheal HAYATU</h2>
+                <h2>Adebayo AJIBADE</h2>
                 <p>
                   <i>-Former student/Lead Engineer at Apple</i>
                 </p>
                 <img
                   width='100px'
-                  src='https://res.cloudinary.com/ddxgfwoao/image/upload/v1593454384/sanusi_Pasport_strd9b.png'
+                  src='https://res.cloudinary.com/ddxgfwoao/image/upload/v1593457391/ajibade_head_hlcqmn.png'
                   alt=''
                 />
               </div>
             </div>
+            <a
+              className='carousel-control-prev'
+              href='#carouselExampleControls'
+              role='button'
+              data-slide='prev'
+            >
+              <span
+                className='carousel-control-prev-icon'
+                aria-hidden='true'
+              ></span>
+              <span className='sr-only'>Previous</span>
+            </a>
+            <a
+              className='carousel-control-next'
+              href='#carouselExampleControls'
+              role='button'
+              data-slide='next'
+            >
+              <span
+                className='carousel-control-next-icon'
+                aria-hidden='true'
+              ></span>
+              <span className='sr-only'>Next</span>
+            </a>
           </div>
         </Testimonial>
       </main>
